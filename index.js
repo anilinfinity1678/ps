5b13593a7e7a6ccf5600140d84e64a223cc1dd90
anilinfinity1678 <94840674+anilinfinity1678@users.noreply.github.com>
2023-05-30 21:56:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-// Import dependencies
 const express = require("express");
-
-// Create the Express app
 const app = express();
+const port = 3000;
 
-// Set the view engine to EJS
+// Set EJS as the view engine
 app.set("view engine", "ejs");
 
+// Set the views directory
+app.set("views", __dirname + "/view");
+
+// Define a route to render the index.ejs template
+app.get("/", (req, res) => {
+    const tree = {
+        name: "Oak Tree",
+        id: 1,
+        height: 10,
+        scientific: "anil",
+        age: 50,
+        latitude: 37.7749,
+        longitude: -122.4194,
+        carbonEmission: 6.8,
+        AnualCarbon: 0.5,
+        facts: "jfkd,ds,lf.dkxk",
+    };
+    res.render("index", { tree });
+});
+
 // Start the server
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
